dba2a57688e74db5c9aaff183dd65536740b5045
Arthyom <allpipiasaaa@hotmail.com>
2016-05-04 20:46:34 -0500

    controles list

--- PruebasLibroCSharp/PruebasLibroCSharp/Form5.Designer.cs
+++ PruebasLibroCSharp/PruebasLibroCSharp/Form5.Designer.cs
@@ -30,37 +30,173 @@
         {
             this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
             this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // checkedListBox1
             // 
             this.checkedListBox1.FormattingEnabled = true;
-            this.checkedListBox1.Location = new System.Drawing.Point(2, 2);
+            this.checkedListBox1.Location = new System.Drawing.Point(12, 18);
             this.checkedListBox1.Name = "checkedListBox1";
-            this.checkedListBox1.Size = new System.Drawing.Size(121, 259);
+            this.checkedListBox1.Size = new System.Drawing.Size(112, 244);
             this.checkedListBox1.TabIndex = 0;
             this.checkedListBox1.SelectedIndexChanged += new System.EventHandler(this.checkedListBox1_SelectedIndexChanged);
             // 
             // listBox1
             // 
             this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(129, -3);
+            this.listBox1.Location = new System.Drawing.Point(130, 11);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(120, 264);
+            this.listBox1.Size = new System.Drawing.Size(107, 251);
             this.listBox1.TabIndex = 1;
             this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 268);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(112, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(130, 268);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(112, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(0, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 295);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(112, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "agregar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 382);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(112, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "bajar";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(12, 353);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(112, 23);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "subir";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(12, 324);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(112, 23);
+            this.button5.TabIndex = 8;
+            this.button5.Text = "quitar";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(130, 295);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(112, 23);
+            this.button6.TabIndex = 9;
+            this.button6.Text = "agregar";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(130, 382);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(112, 23);
+            this.button7.TabIndex = 10;
+            this.button7.Text = "bajar";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(130, 353);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(112, 23);
+            this.button8.TabIndex = 11;
+            this.button8.Text = "subir";
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(130, 324);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(112, 23);
+            this.button9.TabIndex = 12;
+            this.button9.Text = "quitar";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(12, 411);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(230, 23);
+            this.button10.TabIndex = 13;
+            this.button10.Text = "button10";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
             // 
             // Form5
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(254, 263);
+            this.ClientSize = new System.Drawing.Size(247, 437);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.listBox1);
             this.Controls.Add(this.checkedListBox1);
             this.Name = "Form5";
             this.Text = "Form5";
             this.Load += new System.EventHandler(this.Form5_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -68,5 +204,17 @@
 
         private System.Windows.Forms.CheckedListBox checkedListBox1;
         private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
     }
 }
--- PruebasLibroCSharp/PruebasLibroCSharp/Form5.cs
+++ PruebasLibroCSharp/PruebasLibroCSharp/Form5.cs
@@ -20,15 +20,16 @@ namespace PruebasLibroCSharp
         // establecer elementos en los controles 
         private void Form5_Load(object sender, EventArgs e)
         {
+
             // agregar elementos a los listbox 
-            listBox1.Items.Add("Elemento 1");
+            /*listBox1.Items.Add("Elemento 1");
             listBox1.Items.Add("Elemento 2");
             listBox1.Items.Add("Elemento 3");
 
             // agregar elementos al checkbox
             checkedListBox1.Items.Add("Elemento 1");
             checkedListBox1.Items.Add("Elemento 2");
-            checkedListBox1.Items.Add("Elemento 3");
+            checkedListBox1.Items.Add("Elemento 3");*/
 
         }
 
@@ -45,9 +46,62 @@ namespace PruebasLibroCSharp
         // elementos seleccionados en el check
         private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
+            /*
             int indice = checkedListBox1.SelectedIndex;
             MessageBox.Show( checkedListBox1.SelectedIndex.ToString());
-            MessageBox.Show( checkedListBox1.SelectedItem.ToString());
+            MessageBox.Show( checkedListBox1.SelectedItem.ToString());*/
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add( textBox2.Text );
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            checkedListBox1.Items.Add(textBox1.Text);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+            int IndiceSelect = checkedListBox1.SelectedIndex;
+            checkedListBox1.Items.RemoveAt(IndiceSelect);
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            int IndiceSelect = listBox1.SelectedIndex;
+            listBox1.Items.RemoveAt(IndiceSelect);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+            int IndxSelect = checkedListBox1.SelectedIndex;
+            string elemento = checkedListBox1.SelectedItem.ToString();
+
+            checkedListBox1.Items.RemoveAt(IndxSelect);
+            checkedListBox1.Items.Insert(IndxSelect - 1, elemento);
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int IndxSelect = checkedListBox1.SelectedIndex;
+            string elemento = checkedListBox1.SelectedItem.ToString();
+
+            checkedListBox1.Items.RemoveAt(IndxSelect);
+            checkedListBox1.Items.Insert(IndxSelect + 1, elemento);
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+           CheckedListBox.CheckedItemCollection  s = checkedListBox1.CheckedItems;
+
+            foreach (string es in s)
+                listBox1.Items.Add(es);
 
         }
     }
