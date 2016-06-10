c842fe2498ad6e94c137fec0fcd96919ade95993
arthyom <allpipiasaaa@hotmail.com>
2016-06-09 20:49:35 -0500

    terminando la interfaz de dibujo

--- PruebasLibroCSharp/PruebasLibroCSharp/Form11.Designer.cs
+++ PruebasLibroCSharp/PruebasLibroCSharp/Form11.Designer.cs
@@ -30,11 +30,16 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(203, 263);
+            this.button1.Location = new System.Drawing.Point(182, 264);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(292, 23);
             this.button1.TabIndex = 0;
@@ -44,17 +49,64 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(12, 263);
+            this.textBox1.Location = new System.Drawing.Point(4, 264);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(185, 23);
+            this.textBox1.Size = new System.Drawing.Size(172, 23);
             this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(4, 301);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(39, 23);
+            this.textBox2.TabIndex = 2;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(137, 301);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(39, 23);
+            this.textBox3.TabIndex = 3;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(92, 301);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(39, 23);
+            this.textBox4.TabIndex = 4;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(47, 301);
+            this.textBox5.Multiline = true;
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(39, 23);
+            this.textBox5.TabIndex = 5;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(182, 301);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(292, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // Form11
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(498, 382);
+            this.ClientSize = new System.Drawing.Size(478, 330);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
             this.Name = "Form11";
@@ -69,5 +121,10 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Button button2;
     }
 }
--- PruebasLibroCSharp/PruebasLibroCSharp/Form11.cs
+++ PruebasLibroCSharp/PruebasLibroCSharp/Form11.cs
@@ -28,10 +28,28 @@ namespace PruebasLibroCSharp
             Pen lapiz = new Pen(Color.Black);
 
             // crear brocha para el fondo
-            SolidBrush brocha = new SolidBrush(this.ColorFondo);
+            SolidBrush brocha = new SolidBrush(Color.White);
 
             // crear cadena de dibujo para el rectangulo de fondo
             grafico.FillRectangle(brocha, 10, 10, 200, 200);
+
+
+
+
+            // crear una brocha para los colores traseros
+            SolidBrush bt = new SolidBrush(this.ColorTrasero);
+
+            // crear rectangulos trasero
+            grafico.FillRectangle(bt, 50, 50, 100, 100);
+
+
+
+            // crear brochas para el rectangulo primario
+            SolidBrush bf = new SolidBrush(this.ColorFrente);
+
+            // crear el rectangulo frontal
+            grafico.FillRectangle(bf, 60, 60, 150, 150);
+
 
 
 
@@ -51,7 +69,14 @@ namespace PruebasLibroCSharp
 
         private void button1_Click(object sender, EventArgs e)
         {
-            this.ColorFondo = Color.FromName(this.textBox1.Text);
+            // cambiar el color del rectangulo trasero 
+            this.ColorTrasero = Color.FromName(textBox1.Text);
+            Invalidate();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.ColorFrente = Color.FromArgb(Convert.ToInt16(textBox2.Text), Convert.ToInt16(textBox3.Text), Convert.ToInt16(textBox4.Text), Convert.ToInt16(textBox5.Text)) ;
             Invalidate();
         }
     }
