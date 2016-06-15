d23ccce42f8e4c5224d9eaed9a5a058b32fd19bf
arthyom <allpipiasaaa@hotmail.com>
2016-06-14 22:24:30 -0500

    pintando letras

---
+++ PruebasLibroCSharp/PruebasLibroCSharp/Form13.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PruebasLibroCSharp
+{
+    public partial class Form13 : Form
+    {
+        public Form13()
+        {
+            InitializeComponent();
+        }
+
+        private void Form13_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected override void OnPaint ( object sender, DragEventArgs e)
+        {
+            // usar los constructores de font 
+            Font fnt1 = new Font( "Arial",12,FontStyle.Bold | FontStyle.Italic );
+
+        }
+    }
+}
--- PruebasLibroCSharp/PruebasLibroCSharp/Program.cs
+++ PruebasLibroCSharp/PruebasLibroCSharp/Program.cs
@@ -16,7 +16,7 @@ namespace PruebasLibroCSharp
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form12());
+            Application.Run(new Form13());
         }
     }
 }
