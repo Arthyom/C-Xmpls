2ee459c241128ad29cbf8cbc16963dd2a868eaf4
arthyom <allpipiasaaa@hotmail.com>
2016-06-14 22:34:35 -0500

    dibujando letras

--- PruebasLibroCSharp/PruebasLibroCSharp/Form13.cs
+++ PruebasLibroCSharp/PruebasLibroCSharp/Form13.cs
@@ -22,10 +22,25 @@ namespace PruebasLibroCSharp
 
         }
 
-        protected override void OnPaint ( object sender, DragEventArgs e)
+        protected override void OnPaint ( PaintEventArgs e)
         {
+            Graphics     gra = e.Graphics;
+            SolidBrush clr1 = new SolidBrush(Color.Blue);
+            SolidBrush clr2 = new SolidBrush(Color.Red);
+            SolidBrush clr3 = new SolidBrush(Color.Yellow);
+
             // usar los constructores de font 
             Font fnt1 = new Font( "Arial",12,FontStyle.Bold | FontStyle.Italic );
+
+            // usar los constructores de font 
+            Font fnt2 = new Font("Times New Roman", 45, FontStyle.Bold | FontStyle.Italic);
+
+            // usar los constructores de font 
+            Font fnt3 = new Font("Tahoma", 12, FontStyle.Bold | FontStyle.Italic| FontStyle.Strikeout);
+
+            gra.DrawString("este texto sera dibujado", fnt1, clr1, new Point(0,0) );
+            gra.DrawString("este texto sera dibujado despues del anterior como ejemplo", fnt2, clr3, new Point(20, 30));
+            gra.DrawString("este texto sera dibujado", fnt3, clr2, new Point(100, 20));
 
         }
     }
