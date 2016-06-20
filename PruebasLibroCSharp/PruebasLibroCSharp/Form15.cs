8df34a203d4f25ef71e4efd3533be760ffa20da8
arthyom <allpipiasaaa@hotmail.com>
2016-06-20 17:06:24 -0500

    propiedades avanzadas y el metodo invalitate

--- PruebasLibroCSharp/PruebasLibroCSharp/Form15.cs
+++ PruebasLibroCSharp/PruebasLibroCSharp/Form15.cs
@@ -32,18 +32,21 @@ namespace PruebasLibroCSharp
             Graphics objetoGrafico = e.Graphics;
 
             // elementos generales usados en el programa
-            Rectangle dib2 = new Rectangle(12, 34, 100, 140);
-            SolidBrush brocha = new SolidBrush(Color.Red);
+            Rectangle dib2 = new Rectangle(50, 34, 140, 240);
+            Rectangle dib3 = new Rectangle(0, 0, 150, 240);
+            SolidBrush brocha = new SolidBrush(Color.Blue);
             Pen lapiz = new Pen(Color.Black);
             Bitmap mapabit = new Bitmap(10, 10);
-            LinearGradientBrush linard = new LinearGradientBrush(dib2, Color.Red, Color.Black, (float)34.23, false);
+            LinearGradientBrush linard = new LinearGradientBrush(dib2, Color.Red, Color.Black, (float)50.23, true);
 
             // dibujar objetos 
             objetoGrafico.DrawRectangle(lapiz, dib2);
             objetoGrafico.FillRectangle(brocha, dib2);
 
-
-       
+            // dibujar rectangulos con textura
+            objetoGrafico.FillRectangle(linard, dib3);
+            objetoGrafico.DrawRectangle( new Pen(Color.Green,3), dib3);
+        
 
 
 
