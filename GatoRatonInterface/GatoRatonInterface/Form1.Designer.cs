2ef70b1f8771dc796f195df2728b639e48a53bdc
arthyom <allpipiasaaa@hotmail.com>
2016-08-12 21:58:43 -0500

    una interfaz para las 8 reinas

---
+++ GatoRatonInterface/GatoRatonInterface/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace GatoRatonInterface
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.PanelTablero = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // PanelTablero
+            // 
+            this.PanelTablero.Location = new System.Drawing.Point(12, 12);
+            this.PanelTablero.Name = "PanelTablero";
+            this.PanelTablero.Size = new System.Drawing.Size(290, 286);
+            this.PanelTablero.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(418, 48);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(105, 100);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(418, 154);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(105, 100);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(526, 310);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.PanelTablero);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel PanelTablero;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ GatoRatonInterface/GatoRatonInterface/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace GatoRatonInterface
+{
+    public partial class Form1 : Form
+    {
+        Color or;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            PanelTablero.BackColor = Color.Red;
+            DesplegarTablero();
+        }
+
+     
+        /// Desplegar el tablero con las etiquetas correctas respondiendo a los eventos de raton
+        void DesplegarTablero()
+        {
+
+            int DespX = 0, DespY = 0;
+            int cont = 0;
+
+            for ( int i = 0; i < 8; i ++)
+            {
+                for ( int j = 0; j < 8; j ++)
+                {
+                    // crear prototipo para etiqueta 
+                    cont++;
+                    Label NuevaEtiqueta = new Label();
+                    NuevaEtiqueta.Name = "Et" + i.ToString() + j.ToString();
+
+                    if ( i % 2 == 0)
+                    {
+                        if (cont % 2 == 0)
+                            NuevaEtiqueta.BackColor = Color.White;
+                        else
+                            NuevaEtiqueta.BackColor = Color.Black;
+                    }
+                    else
+                          if (cont % 2 == 0)
+                        NuevaEtiqueta.BackColor = Color.Black;
+                    else
+                        NuevaEtiqueta.BackColor = Color.White;
+
+
+                    NuevaEtiqueta.Location = new Point(0 + DespX, 0 + DespY);
+                    NuevaEtiqueta.Size = new Size( 50,50);
+                    NuevaEtiqueta.Font = new Font("arial", 25, FontStyle.Bold);
+                    NuevaEtiqueta.ForeColor = Color.Red;
+                  //  NuevaEtiqueta.Text = i.ToString() + j.ToString();
+                    NuevaEtiqueta.MouseEnter += MouseEnter_EntrarRaton;
+                    NuevaEtiqueta.MouseLeave += MouseLeft_SalirRaton;
+                    NuevaEtiqueta.MouseClick += MouseClick_Click;
+
+                    DespX += 50;
+
+
+                   PanelTablero.Width =+ DespX;
+                    PanelTablero.Controls.Add(NuevaEtiqueta);
+
+                }
+
+                DespY += 50;
+                DespX = 0;
+            }
+            PanelTablero.Height = DespY;
+
+        }
+
+        /// metodos para cambiar el color 
+        void MouseEnter_EntrarRaton(object sender, EventArgs e)
+        {
+           
+            Label Obejto = (Label)sender;
+            this.or = Obejto.BackColor;
+
+            if (Obejto.Name.StartsWith("Et") == true)
+            {
+                Obejto.BackColor = Color.Blue;
+            }
+                
+        }
+
+        /// metodos para cambiar el color 
+        void MouseLeft_SalirRaton(object sender, EventArgs e)
+        {
+            Label Obejto = (Label)sender;
+
+            if (Obejto.Name.StartsWith("Et"))
+            {
+                Obejto.BackColor = this.or;
+            }
+        }
+
+        // click
+        void MouseClick_Click ( object sender, MouseEventArgs e)
+        {
+            Label Obejto = (Label)sender;
+
+            if (Obejto.Name.StartsWith("Et"))
+            {
+                Obejto.Text = "R";
+ 
+
+            }
+        }
+    }
+}
