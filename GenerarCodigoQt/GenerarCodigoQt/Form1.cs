8bd490217f7786930ee60e328b9086b2ed399757
arthyom <allpipiasaaa@hotmail.com>
2016-08-16 23:12:41 -0500

    usando un qr

---
+++ GenerarCodigoQt/GenerarCodigoQt/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Gma.QrCodeNet.Encoding;
+using System.Drawing.Drawing2D;
+using System.IO;
+
+
+
+namespace GenerarCodigoQt
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+ 
+
+            var qrEncoder = new QrEncoder(ErrorCorrectionLevel.H);
+            var qrCode = qrEncoder.Encode("http://blog.koalite.com");
+
+            var renderer = new GraphicsRenderer(new FixedModuleSize(5, QuietZoneModules.Two), Brushes.Black, Brushes.White);
+            using (var stream = new FileStream("qrcode.png", FileMode.Create))
+                renderer.WriteToStream(qrCode.Matrix, ImageFormat.Png, stream);
+
+        }
+    }
+}
