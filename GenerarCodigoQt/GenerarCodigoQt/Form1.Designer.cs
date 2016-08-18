ccb81deeb603a576d527b1606829c69e842414af
arthyom <allpipiasaaa@hotmail.com>
2016-08-17 23:58:27 -0500

    encontrando una nueva manera

--- GenerarCodigoQt/GenerarCodigoQt/Form1.Designer.cs
+++ GenerarCodigoQt/GenerarCodigoQt/Form1.Designer.cs
@@ -28,21 +28,43 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(13, 13);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(245, 156);
+            this.panel1.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 176);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(245, 20);
+            this.textBox1.TabIndex = 1;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(270, 269);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.panel1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
--- GenerarCodigoQt/GenerarCodigoQt/Form1.cs
+++ GenerarCodigoQt/GenerarCodigoQt/Form1.cs
@@ -6,8 +6,8 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+
 using Gma.QrCodeNet.Encoding;
-using System.Drawing.Drawing2D;
 using System.IO;
 
 
@@ -23,15 +23,12 @@ namespace GenerarCodigoQt
 
         private void Form1_Load(object sender, EventArgs e)
         {
- 
 
-            var qrEncoder = new QrEncoder(ErrorCorrectionLevel.H);
-            var qrCode = qrEncoder.Encode("http://blog.koalite.com");
+            QrEncoder Codificador = new QrEncoder(ErrorCorrectionLevel.H);
+            QrCode Codigo = new QrCode();
 
-            var renderer = new GraphicsRenderer(new FixedModuleSize(5, QuietZoneModules.Two), Brushes.Black, Brushes.White);
-            using (var stream = new FileStream("qrcode.png", FileMode.Create))
-                renderer.WriteToStream(qrCode.Matrix, ImageFormat.Png, stream);
-
+            Codificador.TryEncode(textBox1.Text, out Codigo);
+          
         }
     }
 }
