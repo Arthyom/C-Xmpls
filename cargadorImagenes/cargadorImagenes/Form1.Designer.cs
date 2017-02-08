a6190ddaeaaa507a745778defeebe8c8f76d5dbb
arthyom <allpipiasaaa@hotmail.com>
2017-02-07 21:15:13 -0600

    Una primera version, con el circulo dibujado

---
+++ cargadorImagenes/cargadorImagenes/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace cargadorImagenes
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.brillo = new System.Windows.Forms.CheckBox();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.pictureBox1.Location = new System.Drawing.Point(2, 93);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(506, 454);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(2, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(82, 41);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Cargar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.pictureBox2.Location = new System.Drawing.Point(544, 93);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(506, 454);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox2_MouseDown);
+            this.pictureBox2.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox2_MouseMove);
+            this.pictureBox2.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox2_MouseUp);
+            // 
+            // brillo
+            // 
+            this.brillo.AutoSize = true;
+            this.brillo.Location = new System.Drawing.Point(963, 15);
+            this.brillo.Name = "brillo";
+            this.brillo.Size = new System.Drawing.Size(48, 17);
+            this.brillo.TabIndex = 3;
+            this.brillo.Text = "Brillo";
+            this.brillo.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(90, 2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(76, 41);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1055, 559);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.brillo);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.CheckBox brillo;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ cargadorImagenes/cargadorImagenes/Form1.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace cargadorImagenes
+{
+
+
+    public partial class Form1 : Form
+    {
+        public string rutaEntrada;
+        public Bitmap imagenSalida;
+        public Bitmap imagenEntrad;
+        public Point  puntoInicio;
+        public Point  puntoFinal;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // crear imagen negra y la regresa 
+        public Bitmap CrearNegra(int w, int h)
+        {
+            Bitmap negra = new Bitmap(w, h);
+
+            for (int i = 0; i < h; i++)
+                for (int j = 0; j < w; j++)
+                    negra.SetPixel(j, i, Color.Black);
+
+            return negra;
+        }
+
+
+        // cargar la imagen y convertirla en escala de grices 
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            // buscar imagen y cargarla en picture box de entrada
+            OpenFileDialog op = new OpenFileDialog();
+            op.ShowDialog();
+            this.rutaEntrada = op.FileName;
+            this.pictureBox1.Image = Image.FromFile(rutaEntrada);
+            this.imagenEntrad = (Bitmap)pictureBox1.Image;
+
+            // crear una imagen para el picture box de salida
+            this.imagenSalida = new Bitmap ( this.imagenEntrad.Width, this.imagenEntrad.Height);
+
+            // mover los pixeles del pictures box de salida y volverlos escala de grices
+            for ( int i = 0; i < this.imagenSalida.Height; i ++ )
+            {
+                for (int j = 0; j < this.imagenSalida.Width; j++)
+                {
+                    Color c = this.imagenEntrad.GetPixel(j, i);
+                    int  g = (int) ((c.R * 0.30) + (c.G * 0.59) + (c.B * 0.11)); 
+
+                    this.imagenSalida.SetPixel(j, i, Color.FromArgb(g,g,g)) ;
+                }
+            }
+            
+            // cargar la imagen en el picture box de salida
+            this.pictureBox2.Image = this.imagenSalida;
+
+        
+            
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        // mover el centro del circulo
+        private void pictureBox2_MouseMove(object sender, MouseEventArgs e)
+        {
+            // mover el centro del circulo cuando el centro se mueva
+            if (e.Button == MouseButtons.Left)
+            {
+
+                // usar ec. canonica para identificar todos los puntos
+                // (x-h)2 + (y-k)^2 = r2
+                // si el punto i,j satisface la ecuacion para el centro h,k entonces i,j pertenece a la circunferencia
+                double r = 150;
+                int h = e.X, k = e.Y;
+                int nivBrillo = 255;
+
+                // crear un efecto lampara
+                this.pictureBox2.Image = CrearNegra(this.imagenEntrad.Width, this.imagenEntrad.Height);
+                Bitmap aux = (Bitmap)pictureBox2.Image;
+
+                for (int i = 0; i < this.imagenSalida.Height; i++)
+                {
+                    for (int j = 0; j < this.imagenSalida.Width; j++, nivBrillo -= 1)
+                    {
+                        Color c = this.imagenSalida.GetPixel(j, i);
+
+                        // verificar las condiciones de la ecuacion 
+                        if (Math.Pow((j - h), 2) + Math.Pow((i - k), 2) <= Math.Pow(r, 2))
+                        {
+                            int g = (int)((c.R * 0.30) + (c.G * 0.59) + (c.B * 0.11));
+
+                            if (255 <= (g + 255))
+                                g = 255;
+                            else
+                            {
+                                if ((g + nivBrillo) <= 0)
+                                    g = 0;
+                                else
+                                    g += nivBrillo;
+                            }
+
+
+                            aux.SetPixel(j, i, Color.FromArgb(g,g,g));
+
+                        }
+                        else
+                        {
+
+                            aux.SetPixel(j, i, Color.Black);
+
+                        }
+                    }
+                }
+
+                this.pictureBox2.Image = aux;
+                this.pictureBox2.Refresh();
+            }
+        }
+
+        private void pictureBox2_MouseDown(object sender, MouseEventArgs e)
+        {
+        }
+
+        private void pictureBox2_MouseUp(object sender, MouseEventArgs e)
+        {
+            
+        }
+
+        // dibujar una circunferencia en el punto dado
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // usar ec. canonica para identificar todos los puntos
+            // (x-h)2 + (y-k)^2 = r2
+            // si el punto i,j satisface la ecuacion para el centro h,k entonces i,j pertenece a la circunferencia
+            double r = 150;
+            int h = 200, k = 200;
+            int nivBrillo = 10;
+
+            // crear un efecto lampara
+            this.pictureBox2.Image = CrearNegra(this.imagenEntrad.Width, this.imagenEntrad.Height);
+            Bitmap aux = (Bitmap)pictureBox2.Image;
+
+            for (int i = 0; i < this.imagenSalida.Height; i++)
+            {
+                for (int j = 0; j < this.imagenSalida.Width; j++)
+                {
+                    Color c = this.imagenSalida.GetPixel(j, i);
+
+                    // verificar las condiciones de la ecuacion 
+                    if (Math.Pow((j - h), 2) + Math.Pow((i - k), 2) <= Math.Pow(r, 2))
+                    {
+                        int g = (int)((c.R * 0.30) + (c.G * 0.59) + (c.B * 0.11));
+
+                        if (255 <= (g + 255))
+                            g = 255;
+                        else
+                        {
+                            if ((g + nivBrillo) <= 0)
+                                g = 0;
+                            else
+                                g += nivBrillo;
+                        }
+
+
+                        aux.SetPixel(j, i, Color.FromArgb(g, g, g));
+
+                    }
+                    else
+                    {
+
+                        aux.SetPixel(j, i, Color.Black);
+
+                    }
+                }
+            }
+
+            this.pictureBox2.Image = aux;
+            this.pictureBox2.Refresh();
+        }
+    }
+}
