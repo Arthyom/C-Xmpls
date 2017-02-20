0a571577e1c32ba0aa08d8576007b408f7346e06
arthyom <allpipiasaaa@hotmail.com>
2017-02-19 18:57:46 -0600

    ajuste lineal

---
+++ cargadorImagenes/cargadorImagenes/Form2.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace cargadorImagenes
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pictureBox1.Location = new System.Drawing.Point(1, 2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(301, 170);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // chart1
+            // 
+            this.chart1.Location = new System.Drawing.Point(308, 2);
+            this.chart1.Name = "chart1";
+            this.chart1.Size = new System.Drawing.Size(307, 170);
+            this.chart1.TabIndex = 1;
+            this.chart1.Text = "chart1";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.DarkGoldenrod;
+            this.pictureBox2.Location = new System.Drawing.Point(1, 178);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(614, 398);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(619, 578);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
---
+++ cargadorImagenes/cargadorImagenes/Form2.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace cargadorImagenes
+{
+    public partial class Form2 : Form
+    {
+        Bitmap ImagenContraste;
+
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            // mostrar la grafica del histograma
+            this.chart1.ChartAreas.Add("Area1");
+        }
+
+        public double desvEst(int[] vectOc, Bitmap imEt)
+        {
+            double m = 0.0;
+
+            double p = promedio(vectOc, imEt);
+            foreach (double k in vectOc)
+                m += Math.Pow((k - p), 2);
+            m /= vectOc.Length - 1;
+
+            return m;
+        }
+
+        public double promedio(int[] vectOc, Bitmap imEt)
+        {
+            double m = 0.0;
+            foreach (double k in vectOc)
+                m += k;
+
+            m /= (imEt.Width*imEt.Height);
+            return m;
+        }
+
+        // crear la grafica del histograma 
+        public void GraficarHisto(Bitmap ImagenEntrada, int [] vectHist, int b)
+        {
+
+            int offY = 5;
+            int offX = 10;
+
+            // crear imagen gde grafica de las mismas dimenciones que la imagen de entrada
+           // this.ImagenContraste = new Bitmap(ImagenEntrada.Width + offX, ImagenEntrada.Height + 2* offY);
+
+            int Ax = (this.ImagenContraste.Width / 255);
+            double max = (vectHist.Max()/( (double)(ImagenEntrada.Height * ImagenEntrada.Width) ));
+
+            if ( max  < 0.09 ) 
+            {
+                 // iterar para cada valor en el vector 
+                for (int i = 0; i < vectHist.Length; i++)
+                {
+
+                    double fx = ImagenEntrada.Height /(double) vectHist.Max();
+                    double h = vectHist[i] * fx;
+
+                    // recorrer de abajo hacia arriba 
+                    for (int y = 0 ; y < h ; y++)
+                    {
+                        // recorrer en x 
+                        int transY = (int)(this.ImagenContraste.Height - h);
+                        for (int x = ((i) * Ax); x < ((i + 1) * Ax); x++)
+                            this.ImagenContraste.SetPixel(x + offX, (int)(y + transY), Color.Green);
+                    }
+                }
+            }
+            else
+            {
+                 // iterar para cada valor en el vector 
+                for (int i = 0; i < vectHist.Length; i++)
+                {   
+                    double fx = (vectHist[i]/ (double)(ImagenEntrada.Height * ImagenEntrada.Width)) ;
+                    double h = this.ImagenContraste.Height * fx;
+
+                    // recorrer de abajo hacia arriba 
+                    for (int y = 0 ; y < h ; y++)
+                    {
+                        // recorrer en x 
+                        int transY = (int)(this.ImagenContraste.Height - h);
+                        for (int x = ((i) * Ax); x < ((i + 1) * Ax); x++)
+                            this.ImagenContraste.SetPixel(x + offX, (int)(y + transY), Color.Green);
+                    }
+                }
+            }
+            pictureBox1.Refresh();
+            this.ImagenContraste.Save(@"C:\Users\Public\Pictures\Sample Pictures\Prueva2\histo" + b.ToString() + ".jpeg");     
+        }
+
+        // crear la grafica del histograma 
+        public void GraficarHistoAcum(Bitmap ImagenEntrada, int[] vectHist, int b)
+        {
+
+            int offY = 5;
+            int offX = 10;
+            // crear imagen gde grafica de las mismas dimenciones que la imagen de entrada
+            this.ImagenContraste = new Bitmap(ImagenEntrada.Width + offX, ImagenEntrada.Height + 2 * offY);
+
+            int Ax = (this.ImagenContraste.Width / 255);
+            double max = (vectHist.Max() / ((double)(ImagenEntrada.Height * ImagenEntrada.Width)));
+
+            if (max < 0.09)
+            {
+                // iterar para cada valor en el vector 
+                for (int i = 0; i < vectHist.Length; i++)
+                {
+
+                    double fx = ImagenEntrada.Height / (double)vectHist.Max();
+                    double h = vectHist[i] * fx;
+
+                    // recorrer de abajo hacia arriba 
+                    for (int y = 0; y < h; y++)
+                    {
+                        // recorrer en x 
+                        int transY = (int)(this.ImagenContraste.Height - h);
+                        for (int x = ((i) * Ax); x < ((i + 1) * Ax); x++)
+                            this.ImagenContraste.SetPixel(x + offX, (int)(y + transY), Color.Red);
+                    }
+                }
+            }
+            else
+            {
+                // iterar para cada valor en el vector 
+                for (int i = 0; i < vectHist.Length; i++)
+                {
+                    double fx = (vectHist[i] / (double)(ImagenEntrada.Height * ImagenEntrada.Width));
+                    double h = this.ImagenContraste.Height * fx;
+
+                    // recorrer de abajo hacia arriba 
+                    for (int y = 0; y < h; y++)
+                    {
+                        // recorrer en x 
+                        int transY = (int)(this.ImagenContraste.Height - h);
+                        for (int x = ((i) * Ax); x < ((i + 1) * Ax); x++)
+                            this.ImagenContraste.SetPixel(x + offX, (int)(y + transY), Color.Red);
+                    }
+                }
+            }
+
+
+            pictureBox1.Image = this.ImagenContraste;
+            pictureBox2.Refresh();
+            this.ImagenContraste.Save(@"C:\Users\Public\Pictures\Sample Pictures\Prueva2\histo" + b.ToString() + ".jpeg");
+
+        }
+
+        public void InsertarImagen(Bitmap ImagenEntrada)
+        {
+            this.pictureBox2.Image = ImagenEntrada;
+        }
+
+        // crear un metodo para visualizar un histograma 
+        public int[] Histograma(Bitmap ImagenEntrada)
+        {
+
+            int [] h  = new int [256];
+                for (int i = 0; i < ImagenEntrada.Height; i++)
+                {
+                    for (int j = 0; j < ImagenEntrada.Width; j++)
+                    {
+                        Color c = ImagenEntrada.GetPixel(j, i);
+                        int s = (int)((c.R * 0.30) + (c.G * 0.59) + (c.B * 0.11));
+                        h[s] += 1;
+                    }
+                }
+                return h;
+        }
+
+        public int[] HistoAcum(Bitmap ImagenEntrada, int [] h)
+        {
+
+            int[] hC = new int[256];
+            for (int i = 1; i < hC.Length; i++)
+                hC[i] = h[i]+ hC[i - 1];
+            return hC;
+        }
+        
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        public void GuardarImagen(string rutaGuardado, int b)
+        {
+            
+        }
+    }
+}
